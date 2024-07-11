354b4df8805bd7d0f356d8cf5a7dcd55e099ec51
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-07-11 23:53:11 +0530

    Commit

--- Day2/backend/index.js
+++ Day2/backend/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.post('/login',(req,res)=>{
-    
-})
+app.use(express.json());
+
+app.post('user/signup',(req,res)=>{
+
+});
+app.post('user/login',(req,res)=>{
+
+});
 
 app.listen(PORT,()=>{
     console.log(`App is running at port ${PORT}.`);
