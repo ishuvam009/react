2d945ba4585a70c0c646021b3354efe460934275
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-07-14 00:41:45 +0530

    Commit

--- Day3/frontend/src/App.jsx
+++ Day3/frontend/src/App.jsx
@@ -1,35 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Components/>
+      <Header title={'My name is Shuvam'}/>
     </>
+
   )
 }
+
+function Components(){
+  
+  const [a, setA] = useState(0)
+  const [title, setTitle] = useState("The number is "+a)
+
+  function ChangeTitle(){
+    setTitle("The number is "+a)
+    setA(a+1)
+  }
+
+  return(
+    <>
+      <button onClick={ChangeTitle}>Click to change title</button>
+      <Header title={title}/>
+    </>
+  )
+
+}
+
+
+function Header({title}){
+  return <div>
+    {title}
+  </div>
+}  
 
 export default App
