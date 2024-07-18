7fd8944148bb003ff008b7a69edcd9b5daa3c2d6
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-07-18 21:55:48 +0530

    Commit

--- EXP/frontend/src/App.jsx
+++ EXP/frontend/src/App.jsx
@@ -5,6 +5,15 @@ import MediaCard from './components/RecipeReviewCard'
 function App() {
   const [count, setCount] = useState(0)
 
+  const imageUrls = [
+    "https://plus.unsplash.com/premium_photo-1661295611069-249772541426", // Default image
+    "https://images.unsplash.com/photo-1692289748036-837e9b86e7ed",
+    "https://images.unsplash.com/photo-1711618103039-4ca235fbf8e4",
+    "https://images.unsplash.com/photo-1692195010888-8aaf4bd3b9ea",
+    "https://images.unsplash.com/photo-1500044744318-d290fb890942",
+    "https://images.unsplash.com/photo-1678670544297-f00b865be601", 
+  ];
+
   return (
     <>
       <div className="card-container"> {/* Apply styles in App.css */}
