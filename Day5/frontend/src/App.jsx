1046e1b4c60cf861b5960a1053038659cecc70e9
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-07-21 01:41:21 +0530

    Commit

--- Day5/frontend/src/App.jsx
+++ Day5/frontend/src/App.jsx
@@ -2,9 +2,28 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [id, selectedId] = useState(1)
+
   return (
     <div>
-      <Todo id={4} />
+
+      <button onClick={function(){
+        selectedId(1)
+      }}>1</button>
+
+      <button onClick={function(){
+        selectedId(2)
+      }}>2</button>
+
+      <button onClick={function(){
+        selectedId(3)
+      }}>3</button>
+
+    <button onClick={function(){
+        selectedId(4)
+      }}>4</button>
+
+      <Todo id={id}/>
     </div>
   );
 }
@@ -13,12 +32,12 @@ function Todo({ id }) {
   const [todo, setTodo] = useState({});
 
   useEffect(() => {
-    axios.get("https://sum-server.100xdevs.com/todo?id=" + id)
+    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
       .then(response => {
         setTodo(response.data.todo);
       })
       .catch(error => console.error("The error is:", error));
-  }, []); // Include id in the dependency array
+  }, [id]); // Include id in the dependency array
 
   return (
     <div>
