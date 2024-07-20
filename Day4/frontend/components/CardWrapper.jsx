e22970020fc5e842746da99a0443755d25ab8d4c
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-07-20 11:44:35 +0530

    Commit

--- Day4/frontend/components/CardWrapper.jsx
+++ Day4/frontend/components/CardWrapper.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 function Cardwrapper(){
     return (
-        <div>
+        <div style={{display: "flex"}}>
             <CardComponent>
                 <div style={{margin: 10}}>
                     <h1>Hello</h1>
@@ -10,6 +10,19 @@ function Cardwrapper(){
                     <h3>Time 10 pm to 12am</h3>
                 </div>
             </CardComponent>
+
+            <CardComponent>
+                <div>
+                    <img style={{width: 180, padding: 10}} src="https://media.defense.gov/2021/Sep/24/2002860968/1200/1200/0/210919-F-AF248-9084.JPG" alt="f35" />
+                </div>
+            </CardComponent>
+
+            <CardComponent>
+                <div>
+                    <img style={{width: 180, padding: 10}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/SU-30MKI-g4sp_-_edit_2%28clipped%29.jpg/450px-SU-30MKI-g4sp_-_edit_2%28clipped%29.jpg" alt="suk 30 mki" />
+                </div>
+            </CardComponent>
+
         </div>
     )
 }
@@ -17,7 +30,7 @@ function Cardwrapper(){
 
 function CardComponent({children}){
     return (
-        <div style={{border: "2px solid black", width: 200}}>
+        <div style={{border: "2px solid black", width: 200, margin: 8}}>
                     {children}
             </div>
     )
