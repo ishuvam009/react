7c434d429ec96eb694eeb135edda176b3fecb66c
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-07-26 21:52:21 +0530

    Commit

---
+++ Day6/frontend/src/components/Two.jsx
@@ -0,0 +1,12 @@
+import { useMemo, useState } from "react"
+
+function Two(){
+    const [bankBalance, setBankBalance] = useState({})
+    const [cryptoBalance, setCryptoBalance] = useState({})
+
+    useMemo(()=>{
+        
+    },[])
+}
+
+export default Two;
