1cb3315032067dc0d4d6513e9725e9d48dd0e73f
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-07-26 23:53:48 +0530

    Commit

--- Day6/frontend/src/components/Two.jsx
+++ Day6/frontend/src/components/Two.jsx
@@ -1,12 +1,23 @@
-import { useMemo, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 function Two(){
-    const [bankBalance, setBankBalance] = useState({})
-    const [cryptoBalance, setCryptoBalance] = useState({})
+    const [bankBalance, setBankBalance] = useState()
+    //const [cryptoBalance, setCryptoBalance] = useState({})
 
-    useMemo(()=>{
-        
+    useEffect(()=>{
+        setBankBalance(100)
     },[])
+
+    const gains = 500;
+    const totalGains = gains + bankBalance;
+
+    return(
+        <>
+        <div>
+            <h1>Your money is : {totalGains}</h1>
+        </div>
+        </>
+    )
 }
 
 export default Two;
--- Day6/frontend/src/main.jsx
+++ Day6/frontend/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import One from './components/One.jsx'
+import Two from './components/Two.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Two/>
   </React.StrictMode>,
 )
