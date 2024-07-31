efcf258a9635187afe74d660a4306429e6b2bb41
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-08-01 02:24:04 +0530

    Commit

--- Day7/react-router/src/App.jsx
+++ Day7/react-router/src/App.jsx
@@ -1,34 +1,41 @@
 import { Dashboard } from "./pages/Dashboard"
 import { Landing } from "./pages/Landing"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
 import './App.css'
 
 function App() {
   
   return (
     <>
-    <div style={{display: "flex", justifyContent: 'space-around',width: '100%', height: 40, backgroundColor:'#f5f5dc'}}>
-
-        <button onClick={()=>{
-          window.location.href="/"
-        }}>Landing Page</button>
-        <button onClick={()=>{
-          window.location.href="/dashboard"
-        }}>Dashboard</button>
-        <p>Home</p>
-        <p>Career</p>
-        <p>About Us</p>
-      
-      <p></p>
-    </div>
-
       <BrowserRouter>
+        <Appbar/>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </>
+  )
+}
+
+
+function Appbar(){
+
+  const navigate= useNavigate();
+  return(
+    <div style={{display: "flex", justifyContent: 'space-around',width: '100%', height: 40, backgroundColor:'#f5f5dc'}}>
+
+        <button onClick={()=>{
+          navigate("/")
+        }}>Landing Page</button>
+        <button onClick={()=>{
+          navigate("/dashboard")
+        }}>Dashboard</button>
+        <a href="/"><p>Home</p></a>
+        <p>Career</p>
+        <p>About Us</p>
+      
+    </div>
   )
 }
 
