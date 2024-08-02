13e5cc0c25e7287844ee76303c74a3592da4b7dd
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-08-02 12:03:30 +0530

    Commit

--- Day7/react-router/src/components/Props.jsx
+++ Day7/react-router/src/components/Props.jsx
@@ -1,5 +1,32 @@
-function Props(){
+import { useState } from "react";
 
+function Props(){
+    const [count, setCount] = useState(0)
+
+    return(
+        <div>
+            <Count count={count}/> <br />
+            <Button count={count} setCount={setCount}/>
+        </div>
+    )
+}
+
+function Count({count}){
+return (
+    <div>
+        Current count is: {count}
+    </div>
+)
+}
+
+
+function Button({count, setCount}){
+return(
+    <div>
+        <button onClick={()=>{ setCount(count+1)}}>Increase</button> 
+        <button onClick={()=>{ setCount(count-1)}}>Decrease</button>
+    </div>
+)
 }
 
 export default Props;
