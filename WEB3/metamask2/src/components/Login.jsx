43723b9884483613973f6533a68ca0cecfefb0df
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-08-30 23:35:05 +0530

    Commit

--- WEB3/metamask2/src/components/Login.jsx
+++ WEB3/metamask2/src/components/Login.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
+import Header from "./Header";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -18,25 +19,26 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:3000/login', {
+      const response = await axios.post("http://localhost:3000/login", {
         username,
         password,
       });
 
       if (response.status === 200) {
         // Handle successful login, e.g., store the token, redirect, etc.
-        console.log('Login successful:', response.data);
+        console.log("Login successful:", response.data);
         // Redirect or update the UI accordingly
       }
     } catch (error) {
-      setErrorMessage('Login failed. Please check your username and password.');
-      console.error('Error during login:', error);
+      setErrorMessage("Login failed. Please check your username and password.");
+      console.error("Error during login:", error);
     }
   };
 
   return (
     <>
-      <div className="pt-10">
+      <Header />
+      <div className="pt-16">
         <div className="mx-auto max-w-sm rounded-sm shadow-xl bg-white">
           <p className="font-bold text-2xl text-center">Login</p>
           <form
--- WEB3/metamask2/src/components/SignUp.jsx
+++ WEB3/metamask2/src/components/SignUp.jsx
@@ -1,7 +1,17 @@
-export default function SignUp(){
-    return(
-        <>
-            <div></div>
-        </>
-    )
+import Header from "./Header";
+
+export default function SignUp() {
+  return (
+    <>
+      <Header />
+      <div className="pt-16">
+        <div className="mx-auto max-w-sm rounded-sm shadow-xl bg-white">
+          <p className="font-bold text-2xl text-center">SignUp</p>
+          <form action="">
+            
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
